2fa1867f1f45f90cca1b38f43132bdff90964177
puneetsachdeva-number7even <puneet@number7even.de>
2018-05-04 15:01:24 +0530

    fixing image display

--- app/Http/Controllers/FrontEnd/PropertyController.php
+++ app/Http/Controllers/FrontEnd/PropertyController.php
@@ -501,7 +501,7 @@ class PropertyController extends Controller {
                 $image = Image::make($remoteImage)->response('jpg');
             }
 			$image = Image::make($remoteImage)->resize(600, 600)->response('jpg');*/
-			$image = $propertyImage->folder_src.'/emporium-voyage_hotelname.jpg';
+			$image = Image::make($propertyImage->folder_src.'/emporium-voyage_hotelname.jpg')->response('jpg');
             return $image;
 
 		}
