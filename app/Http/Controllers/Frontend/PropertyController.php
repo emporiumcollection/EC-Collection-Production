7fc8b5e720d1fb408529bacf9cd381e013cc15b4
kumar gaurav <gaurav.kumar1@mail.vinove.com>
2018-06-04 13:59:51 +0530

    new code

--- app/Http/Controllers/Frontend/PropertyController.php
+++ app/Http/Controllers/Frontend/PropertyController.php
@@ -19,9 +19,10 @@ class PropertyController extends Controller {
 	
 	public function getPropertyGridListByCategory(Request $request)
 	{
+		dd("hello");
 		$this->data['slug'] = $request->slug;
 		$this->data['dateslug'] = '';
-		dd($this->data);
+
         $this->data['slider'] = \DB::table('tb_sliders')->select('slider_category','slider_title','slider_description','slider_img','slider_link','slide_type')->where('slider_category', $request->slug)->where('slider_status',1)->orderBy('sort_num','asc')->get();
 
           $this->data['destination_category'] =0;
