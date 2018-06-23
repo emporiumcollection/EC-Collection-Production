c12d13499213418fdce071b1c69805eeeefa3bf2
number7even-dev <aman@number7even.de>
2018-06-23 12:38:11 +0530

    Image optimization

--- app/Http/Controllers/Frontend/PropertyController.php
+++ app/Http/Controllers/Frontend/PropertyController.php
@@ -566,16 +566,18 @@ class PropertyController extends Controller {
 		$propertyImage = CustomQuery::getPropertyImage($propid);	
 		$remoteImage = $propertyImage->containerfolder_src;		
 		$propertyNameImg = $propertyImage->containerfolder_path_src.'emporium-voyage_'.$propertyName.'.jpeg';
-		$width = Image::make($remoteImage)->width();
-		$height = Image::make($remoteImage)->height();
 		if(file_exists($propertyNameImg)){
 			header("Content-type: image/jpeg");
 			$data = file_get_contents($propertyNameImg);
 			$image = 'data:image/jpeg;base64,' . base64_encode($data);
 		} else {
 			if(!empty($propertyImage)) {
-				$height1 = 400 * $height /$width;
-				$image = Image::make($remoteImage)->resize(400,$height1)->response('jpg');
+				$tObj = Image::make($remoteImage);
+                $width = $tObj->width();
+                $height = $tObj->height();
+        
+                $height1 = 400 * $height /$width;
+				$image = $tObj->resize(400,$height1)->response('jpg');
 			} else {
 				return false;
 			}
