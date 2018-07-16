c1f557497bd2450c98e9a09db07f060af1cdfc1f
number7even-dev <aman@number7even.de>
2018-07-16 12:30:53 +0530

    helper fuction for grid thumb effects

--- app/Helpers/CustomQuery.php
+++ app/Helpers/CustomQuery.php
@@ -26,9 +26,48 @@ class CustomQuery
 			$proertyObj->containerfolder_src = $proertyObj->containerfolder_path_src.$proertyObj->file_name;			
 		}
 		return $proertyObj;
-
-
     }
-
+    
+    static function getPropertyImagesFromDBByFileId($fileId){
+        $containerObj = new \App\Http\Controllers\ContainerController;
+        $proertyObj = \DB::table('tb_properties_images')->join('tb_container_files', 'tb_container_files.id', '=', 'tb_properties_images.file_id')->select('tb_properties_images.*', 'tb_container_files.file_name', 'tb_container_files.file_size', 'tb_container_files.file_type', 'tb_container_files.folder_id')->where('tb_properties_images.file_id', $fileId)->where('tb_properties_images.type', 'Property Images')->orderBy('tb_container_files.file_sort_num', 'asc')->first();
+        if(!empty($proertyObj))
+		{
+            $proertyObj->folder_src = $containerObj->getThumbpath($proertyObj->folder_id);
+			$proertyObj->img_src = $proertyObj->folder_src.$proertyObj->file_name;
+			$proertyObj->containerfolder_path_src = $containerObj->getContainerUserPath($proertyObj->folder_id);
+			$proertyObj->containerfolder_src = $proertyObj->containerfolder_path_src.$proertyObj->file_name;			
+		}
+		return $proertyObj;
+    }
+    
+    static function getPropertyImagesFromDB($propId,$limit=0,$get_thumb_path=false,$get_containerpath=false){
+		$containerObj = new \App\Http\Controllers\ContainerController;
+        if($limit > 0){
+            $proertyObj = \DB::table('tb_properties_images')->join('tb_container_files', 'tb_container_files.id', '=', 'tb_properties_images.file_id')->select('tb_properties_images.*', 'tb_container_files.file_name', 'tb_container_files.file_size', 'tb_container_files.file_type', 'tb_container_files.folder_id')->where('tb_properties_images.property_id', $propId)->where('tb_properties_images.type', 'Property Images')->orderBy('tb_container_files.file_sort_num', 'asc')->limit($limit)->get();    
+        }else
+        {
+            $proertyObj = \DB::table('tb_properties_images')->join('tb_container_files', 'tb_container_files.id', '=', 'tb_properties_images.file_id')->select('tb_properties_images.*', 'tb_container_files.file_name', 'tb_container_files.file_size', 'tb_container_files.file_type', 'tb_container_files.folder_id')->where('tb_properties_images.property_id', $propId)->where('tb_properties_images.type', 'Property Images')->orderBy('tb_container_files.file_sort_num', 'asc')->get();
+        }
+        
+        $returnObj = array();
+        if(!empty($proertyObj)){
+            foreach($proertyObj as $row){
+                if($get_thumb_path){
+                    $row->folder_src = $containerObj->getThumbpath($row->folder_id);
+                    $row->img_src = $row->folder_src.$row->file_name;
+                }
+                    
+                if($get_containerpath){
+                    $row->containerfolder_path_src = $containerObj->getContainerUserPath($row->folder_id);
+                    $row->containerfolder_src = $row->containerfolder_path_src.$row->file_name;	
+                }
+                
+                $returnObj[] = $row;
+            }
+        }        
+        
+		return $returnObj;
+    }
     
 }
