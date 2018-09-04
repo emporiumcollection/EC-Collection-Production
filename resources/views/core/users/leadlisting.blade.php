cafe675c573da6d2c9343a89f03a23fe672e2ec1
developer-number7even <developert@number7even.de>
2018-09-04 12:43:43 +0530

    fixes

--- resources/views/core/users/leadlisting.blade.php
+++ resources/views/core/users/leadlisting.blade.php
@@ -8,20 +8,7 @@
 .modal-dialog { width:500px !important; }
 </style>
 
-  <div class="page-content row">
-    <!-- Page header -->
-    <div class="page-header">
-      <div class="page-title">
-        <h3> {{ $pageTitle }} <small>{{ $pageNote }}</small></h3>
-      </div>
-
-      <ul class="breadcrumb">
-        <li><a href="{{ URL::to('dashboard') }}"> Dashboard </a></li>
-        <li class="active">{{ $pageTitle }}</li>
-      </ul>	  
-	  
-    </div>
-	
+  
 	
 	<div class="page-content-wrapper m-t">
     <div id="formerrors"></div>
