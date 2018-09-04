546f74fa7f108b3fc2c4d1f56c35e321271e1ae4
developer-number7even <developert@number7even.de>
2018-09-04 12:37:03 +0530

    fixes

--- resources/views/core/users/leadlisting.blade.php
+++ resources/views/core/users/leadlisting.blade.php
@@ -58,51 +58,7 @@
     
     	
     	
-    	 {!! Form::open(array('url'=>'core/users/delete/', 'class'=>'form-horizontal' ,'id' =>'SximoTable' )) !!}
-    	 <div class="table-responsive" style="min-height:300px;">
-        <table class="table table-striped ">
-            <thead>
-    			<tr>
-    				<th class="number"> No </th>
-    				<th> <input type="checkbox" class="checkall" /></th>
-    				
-    				@foreach ($tableGrid as $t)
-    					@if($t['view'] =='1')
-    						<th>{{ $t['label'] }}</th>
-    					@endif
-    				@endforeach
-    				<th width="70" >{{ Lang::get('core.btn_action') }}</th>
-    			  </tr>
-            </thead>
-    
-            <tbody>
-    						
-                
-                  
-            </tbody>
-          
-        </table>
-    	<input type="hidden" name="md" value="" />
-    	</div>
-    	{!! Form::close() !!}
-        
-    	<div class="table-footer">
-        	<div class="row">
-                <div class="col-sm-5">
-                    <div class="table-actions" style=" padding: 10px 0">
-                    
-                    </div>					
-                </div>
-                <div class="col-sm-3">
-                    <p class="text-center" style=" padding: 25px 0">
-            		Total : <b>{{ $pagination->total() }}</b>
-            		</p>
-                </div>
-                <div class="col-sm-4">			 
-        	       {!! $pagination->appends($pager)->render() !!}
-                </div>
-        	</div>
-    	</div>
+    	 
         
     	</div>
     </div>	
