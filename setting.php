44cf2eaa9c97e0151d5c4ac8857f318323daf606
Dalip kumar <dalip.01rad@gmail.com>
2019-01-08 12:35:15 +0530

    update settting.php

--- setting.php
+++ setting.php
@@ -2,12 +2,13 @@
 define('CNF_APPNAME','Emporium Voyage Membership Portal');
 define('CNF_APPDESC','Emporium Voyage Membership Portal');
 define('CNF_COMNAME','Emporium Voyage Membership Portal');
-define('CNF_EMAIL','reservations@emporium-voyage.com');
+define('CNF_EMAIL','marketing@emporium-collection.com');
 define('CNF_METAKEY','Emporium Voyage Membership Portal');
 define('CNF_METADESC','Emporium Voyage Membership Portal');
 define('CNF_GROUP','3');
 define('CNF_ACTIVATION','confirmation');
 define('CNF_MULTILANG','1');
+define('CNF_SUBTRACT_FEE','1');
 define('CNF_LANG','en');
 define('CNF_REGIST','true');
 define('CNF_FRONT','true');
@@ -20,4 +21,5 @@ define('CNF_LOGO','backend-logo.png');
 define('CNF_ALLOWIP','');
 define('CNF_RESTRICIP','192.116.134 , 194.111.606.21 ');
 define('CNF_YOUTUBE_API_KEY','');
+define('CNF_SUPERADMIN_EMAIL','dalip.01rad@gmail.com');
 ?>
