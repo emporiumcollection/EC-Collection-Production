b25bf6b3c70640eccd7f9a156a1fb621d22ffee3
Dalip kumar <dalip.01rad@gmail.com>
2019-01-08 11:59:30 +0530

    update controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -14,7 +14,8 @@ use Validator, Input, Redirect ;
 abstract class Controller extends BaseController {
 
 	use DispatchesJobs, ValidatesRequests;
-
+    
+    var $is_public = true;
 	public function __construct()
 	{
 		
@@ -58,9 +59,54 @@ abstract class Controller extends BaseController {
 		$data = array(
 				'last_activity'=> strtotime(Carbon::now())
 			);
-		\DB::table('tb_users')->where('id',\Session::get('uid'))->update($data);   
-	} 	
-
+		\DB::table('tb_users')->where('id',\Session::get('uid'))->update($data);  
+        
+        $this->checkb2buser(); 
+	}
+    
+    function getPackagesIdsAccordingMembership(){
+        $pakages_arr = array();
+        $is_all = false;
+        $upackages = array();
+        if(isset(\Auth::user()->group_id)){
+            $gid = (int) \Auth::user()->group_id;
+            $travellerId = \CommonHelper::getusertype("users-b2c");
+            if($gid != $travellerId){
+                $is_all = true;
+            }else
+            {
+                $order_pack_results = \DB::table('tb_orders')->select('tb_packages.id','tb_orders.status')
+                                    ->join('tb_order_items','tb_order_items.order_id','=','tb_orders.id')
+                                    ->join('tb_packages','tb_packages.id','=','tb_order_items.package_id')
+                                    ->join('tb_packages_user_groups','tb_packages.id','=','tb_packages_user_groups.package_id')
+                                    ->where('tb_packages_user_groups.group_id',$travellerId)->where('tb_orders.status','Success')
+                                    ->groupBy('tb_packages.id')->get();
+                if(!empty($order_pack_results)){
+                    foreach($order_pack_results as $sii_package){
+                        if(!in_array($sii_package->id,$pakages_arr)){ $pakages_arr[] = $sii_package->id; }
+                    }
+                }
+            }
+        }
+        
+        //get public packages
+        if(count($pakages_arr) <= 0){
+            if($is_all === true){
+                $pakages_results = \DB::table('tb_packages')->select('id')->get();
+            }else
+            {
+                $pakages_results = \DB::table('tb_packages')->select('id')->where('is_public',true)->get();
+            }  
+            if(!empty($pakages_results)){
+                foreach($pakages_results as $si_package){
+                    if(!in_array($si_package->id,$pakages_arr)){ $pakages_arr[] = $si_package->id; }
+                }
+            }
+        }
+        //End
+        
+        return $pakages_arr;
+    }
 
 	function getComboselect( Request $request)
 	{
@@ -594,6 +640,21 @@ abstract class Controller extends BaseController {
 
 		
 	}
+    public function checkb2buser(){
+        $user_info = \DB::table('tb_users')->where('id',\Session::get('uid'))->first();
+        if(\Auth::check()){ 
+            $uid = \Auth::user()->id;
+            
+            if( \Auth::user()->i_agree == 0 ||  \Auth::user()->privacy_policy == 0 ||  \Auth::user()->cookie_policy == 0){
+                  
+            }elseif( \Auth::user()->new_user == 1){ 
+                return Redirect::to('whoiam');
+            }elseif( \Auth::user()->hotel_setup == 0){ 
+                return Redirect::to('properties');
+            }
+            
+        }
+    }
 
 }
 
