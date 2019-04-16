4116155644308cb128f54b03d7ba8367f88809c2
Dalip kumar <dalip.01rad@gmail.com>
2019-04-16 19:33:48 +0530

    destination channel fixes

--- resources/views/frontend/themes/emporium/layouts/sections/social_youtube_sidebar.blade.php
+++ resources/views/frontend/themes/emporium/layouts/sections/social_youtube_sidebar.blade.php
@@ -6,9 +6,26 @@
     </div>
     <div class="mobilemenu-inner">
         <div class="mobilemainnav openmobilemenu">
-            <div class="mobilenavheader " data-option="home" data-option-type="logo"><a href="{{url('/')}}"><img
-                            src="{{ asset('themes/emporium/images/emporium-voyage-logo.png')}}" alt="Emporium Voyage"
-                            class="img-responsive"/></a></div>
+            <div class="mobilenavheader " data-option="home" data-option-type="logo">
+                {{-- <a href="{{url('/')}}">
+                    <img src="{{ asset('themes/emporium/images/emporium-voyage-logo.png')}}" alt="Emporium Voyage" class="img-responsive"/>
+                </a> --}}
+                @if(defined('CNF_FRONTEND_LOGO'))
+                    @if(file_exists(public_path().'/sximo/images/'.CNF_FRONTEND_LOGO) && CNF_FRONTEND_LOGO !='')
+                        <a href="{{url('/')}}">
+                            <img src="{{ asset('sximo/images/'.CNF_FRONTEND_LOGO)}}"  alt="{{ CNF_APPNAME }}" class="img-responsive"/>      
+                        </a>
+                    @else
+                        <a href="{{url('/')}}">
+                            <img src="{{ asset('themes/emporium/images/emporium-voyage-logo.png')}}" alt="Emporium Voyage" class="img-responsive"/>
+                        </a>
+                    @endif
+                @else
+                    <a href="{{url('/')}}">
+                        <img src="{{ asset('themes/emporium/images/emporium-voyage-logo.png')}}" alt="Emporium Voyage" class="img-responsive"/>
+                    </a>
+                @endif
+            </div>
             <div class="mobilenavheader hide" data-option="child-global">
                 <h3 data-option-title="global"></h3>
                 <a  class="homelinknav backtohomelink cursor" data-option-action="back"
@@ -29,8 +46,17 @@
                 </li>
             </ul>
             <ul class="mobilemenulist" data-option="home">
-                <li><a class="cursor" data-action="select-destination-youtube" data-id="0">SELECT YOUR DESTINATION</a></li>
-                <li><a class="cursor" data-action="select-menu" data-position="business" data-id="0">COMPANY</a></li>
+                <!--<li><a class="cursor" data-action="select-destination-youtube" data-id="0">SELECT YOUR DESTINATION</a></li>
+                <li><a class="cursor" data-action="select-menu" data-position="business" data-id="0">COMPANY</a></li>-->
+                <li><a class="cursor" data-action="select-collection">SEARCH OUR COLLECTION</a></li>
+                <li><a class="cursor" data-action="search-by-date">Search availability</a></li>
+                <li><a class="cursor" data-action="select-destination" data-id="0">Search by destination</a></li>
+                <li><a class="cursor" data-action="select-experience" >Search by Experience</a></li>
+                <!--li><a href="javascript:void(0)" >PERSONALIZED SERVICE</a></li-->
+                <!--<li><a class="cursor" data-action="company">COMPANY</a></li>-->
+                <li><a class="cursor" data-action="select-destination-youtube">Search Destination Channel</a></li>
+                <li><a class="cursor" data-action="select-menu" data-position="business" data-id="0">Company & Info</a></li>
+                <li><a class="cursor" href="{{URL::to('memberships')}}" >Membership</a></li>
             </ul>
             <ul class="mobilemenulist hide socialyoutubemenu" data-option="selected-option-list">
             </ul>
