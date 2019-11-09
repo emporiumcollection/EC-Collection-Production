8586d2b0c85be52780b7ac5223e5869b46dd3fad
Dalip Kumar <dalip.01rad@gmail.com>
2019-11-09 16:49:48 +0530

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8919,6 +8919,7 @@ die;        */
         //$post_json = json_encode($arr);
         //$hapikey = readline("Enter hapikey: 94aa9df3-d9f7-48a5-81a3-b365fcbe7492: ");
         $endpoint = 'https://api.hubapi.com/contacts/v1/contact/email/'.$email.'/profile?hapikey='.$hapikey;
+        echo $endpoint; die;
         $ch = @curl_init();
         //@curl_setopt($ch, CURLOPT_POST, true);
         //@curl_setopt($ch, CURLOPT_POSTFIELDS, $post_json);
