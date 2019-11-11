cd3f1d4bfdf4aea27b5c4778e43b4a29065fab8e
Dalip Kumar <dalip.01rad@gmail.com>
2019-11-11 12:08:03 +0530

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8871,7 +8871,7 @@ die;        */
                 //print_r($obj); die;
                 if(array_key_exists('status', $arrRes)){ echo "hhh"; die;
                     $this->hsPostDetails($objUser);    
-                }else{ echo $arrRes['vid']; echo "hkh"; die;
+                }else{ print_r($arrRes); die; echo $arrRes['vid']; echo "hkh"; die;
                     $this->hsPostMergeDetails($obj['vid']);        
                 }        
             }   
