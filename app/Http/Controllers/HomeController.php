539738e4b5d6c90902957c66069730346a596470
Dalip Kumar <dalip.01rad@gmail.com>
2019-11-12 15:20:42 +0530

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9099,14 +9099,14 @@ die;        */
         } 
         
         $arr = array(
-            "associations"=>array("associatedVids"=>array(27136)),
+            "associations"=>array("associatedVids"=>array(501)),
             "properties"=>$mobj   
         );
         //print_r(json_encode($arr)); die;
         
         $hapikey = \Config::get('hubspot.hsApiKey');
         
-        print_r(json_encode($arr)); die;
+        //print_r(json_encode($arr)); die;
         
         $post_json = json_encode($arr); 
         //$hapikey = readline("Enter hapikey: 94aa9df3-d9f7-48a5-81a3-b365fcbe7492: ");
