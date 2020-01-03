a2679289cd38768d509fc3d6f99f12c6a7169772
Dalip Kumar <dalip.01rad@gmail.com>
2020-01-03 13:43:25 +0530

    Update pdp_sidebar.blade.php

--- resources/views/frontend/themes/emporium/layouts/sections/pdp_sidebar.blade.php
+++ resources/views/frontend/themes/emporium/layouts/sections/pdp_sidebar.blade.php
@@ -26,13 +26,11 @@
 				
 				@if($propertyDetail['data']->architecture_title!='' && $propertyDetail['data']->architecture_desciription!='')
 					<li><a href="#design-architecture" class="closenavlink scrollpage">DESIGN & ARCHITECTURE</a></li>
-				@endif
+				@endif				
 				
-				<?php /*
 				@if($propertyDetail['data']->restaurant_ids!='' || $propertyDetail['data']->spa_ids!='' || $propertyDetail['data']->bar_ids!='')
 					<li><a href="{{URL::to($propertyDetail['data']->property_slug.'/restaurant')}}">RESTAURANT & BAR</a></li>
-				@endif 
-				*/ ?>
+				@endif				
 				
 				@if($propertyDetail['data']->video_title!='')
 					<li><a href="#video" class="closenavlink scrollpage">VIDEO</a></li>
