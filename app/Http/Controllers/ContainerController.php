e953663fb3fcf3509b5527bf9b8d93f089369e30
Dalip Kumar <dalip.01rad@gmail.com>
2020-01-14 16:59:49 +0530

    Update ContainerController.php

--- app/Http/Controllers/ContainerController.php
+++ app/Http/Controllers/ContainerController.php
@@ -3584,21 +3584,27 @@ class ContainerController extends Controller {
 		);
 		// Get Query 
 		$results = $this->model->getRows( $params );
-        
-      if(\Auth::user()->group_id==5)
-	  {  
-	       foreach($results['rows'] as $row) {
-    			$parent_folders_array[] = $row;    			
-    	   }
-      }else{
-    	  if ($results) {
-    		foreach($results['rows'] as $row) {
-    			$parent_folders_array[] = $row;
-    			$parent_folders_array = $this->fetchFolderParentListArray($row->parent_id, $parent_folders_array);
-    		}
-    	  }
-       }
       
+      if(\Auth::check() == true)
+      {   
+          if(\Auth::user()->group_id==5)
+    	  {  
+    	       foreach($results['rows'] as $row) {
+        			$parent_folders_array[] = $row;    			
+        	   }
+          }else{
+        	  if ($results) {
+        		foreach($results['rows'] as $row) {
+        			$parent_folders_array[] = $row;
+        			$parent_folders_array = $this->fetchFolderParentListArray($row->parent_id, $parent_folders_array);
+        		}
+        	  }
+           }
+      }else{
+           foreach($results['rows'] as $row) {
+    			$parent_folders_array[] = $row;    			
+    	   }    
+      }
 	  return $parent_folders_array;
 	}
 	
