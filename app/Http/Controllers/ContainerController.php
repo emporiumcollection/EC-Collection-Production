4b0346cd76d0d982ca61fb313d522993c034c27c
Dalip Kumar <dalip.01rad@gmail.com>
2020-01-14 17:35:50 +0530

    Update ContainerController.php

--- app/Http/Controllers/ContainerController.php
+++ app/Http/Controllers/ContainerController.php
@@ -5492,7 +5492,7 @@ class ContainerController extends Controller {
 	
 		$ct=0; 
 		$this->data['rowData'] = array();
-		
+		print_r($this->data['rowData']); die;
 		foreach($results['rows'] as $folderObj ){
 			
 			$totfiles = DB::table('tb_container_files')->select('id')->where('folder_id',$folderObj->id)->count();
