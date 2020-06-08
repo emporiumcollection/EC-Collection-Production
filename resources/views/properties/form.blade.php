c8f07f24b7943c70f95833ca6ca6b0199fb5bc40
Dalip Kumar <dalip.01rad@gmail.com>
2020-06-08 18:00:00 +0530

    Update form.blade.php

--- resources/views/properties/form.blade.php
+++ resources/views/properties/form.blade.php
@@ -314,8 +314,8 @@
 
             /*$("#assigned_user_id").jCombo("{{ URL::to('properties/comboselect?filter=tb_users:id:first_name|last_name') }}",
                     {selected_value: '{{ $property_user }}'});*/
-            $("#assigned_user_id").jCombo("{{ URL::to('properties/comboselect?filter=tb_users:id:first_name|last_name') }}",
-                    {selected_value: '{{ $row->assigned_user_id }}'});
+            //$("#assigned_user_id").jCombo("{{ URL::to('properties/comboselect?filter=tb_users:id:first_name|last_name') }}",
+            //        {selected_value: '{{ $row->assigned_user_id }}'});
 
             $('.removeCurrentFiles').on('click', function () {
                 var removeUrl = $(this).attr('href');
