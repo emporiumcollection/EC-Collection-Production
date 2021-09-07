a598b81ba2519ba26ddd624ce93b61c76e11e2b0
Mahesh Chavda <chavdamahesh@gmail.com>
2021-09-07 19:37:37 +0530

    Removed  yajra

--- config/app.php
+++ config/app.php
@@ -136,7 +136,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Yajra\DataTables\DataTablesServiceProvider::class,
 
         /*
          * Application Service Providers...
