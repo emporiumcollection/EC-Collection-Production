46d84f7edf15d9a74fc2706e4c47348681cc7011
Mahesh Chavda <chavdamahesh@gmail.com>
2021-10-25 19:40:22 +0530

    share link bug fix

--- config/database.php
+++ config/database.php
@@ -54,10 +54,14 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost:3306'),
+            'host'      => env('DB_HOST', 'localhost'),
             'database'  => env('DB_DATABASE', 'empo_voyage'),
             'username'  => env('DB_USERNAME', 'root'),
             'password'  => env('DB_PASSWORD', ''),
+            /*'host'      => env('DB_HOST', '172.105.71.247'),
+            'database'  => env('DB_DATABASE', 'EmporiumVoyage'),
+            'username'  => env('DB_USERNAME', 'voyage'),
+            'password'  => env('DB_PASSWORD', '!!Voyage2021%%$$'),*/
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
