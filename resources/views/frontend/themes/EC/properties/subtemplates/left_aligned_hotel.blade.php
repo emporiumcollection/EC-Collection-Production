c11788210f22d2ad00551eae6d7f430555e28ac5
Mahesh Chavda <chavdamahesh@gmail.com>
2021-11-01 17:23:59 +0530

    Fixed result bo

--- resources/views/frontend/themes/EC/properties/subtemplates/left_aligned_hotel.blade.php
+++ resources/views/frontend/themes/EC/properties/subtemplates/left_aligned_hotel.blade.php
@@ -8,7 +8,7 @@
 <div class="mb-5">
   <div class="row align-items-center">
     <div class="col-md-6">
-      <h3 class="title-second title-line mb-0"><?php echo $editorChoice->detail_section1_title;?></h3>
+      <h3 class="title-second title-line mb-0"><?php echo $editorChoice->property_name;?></h3>
     </div>
     <div class="col-md-6 text-right">
       <div class="d-flex justify-content-end align-items-center">
