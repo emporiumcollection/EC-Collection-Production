b055ae388a28cccd2025392414024ccfd04b92d1
Mahesh Chavda <chavdamahesh@gmail.com>
2021-11-03 13:35:39 +0530

    Added query cache

--- app/Http/Traits/Property.php
+++ app/Http/Traits/Property.php
@@ -209,7 +209,8 @@ trait Property {
         request()->get('atmosphere_ids').
         request()->get('style_ids'));
 
-        return Cache::get($key, function ($keyword) {
+        return Cache::get($key, function () {
+            $keyword = request()->get('s');
             //Query starts
             $experience_id = false;
             if(request()->get('experience')){
