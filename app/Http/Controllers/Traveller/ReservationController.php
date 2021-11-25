99316726565c5d84d86f8648792a6995f740b446
Mahesh Chavda <chavdamahesh@gmail.com>
2021-11-25 10:46:25 +0530

    Code fixes

--- app/Http/Controllers/Traveller/ReservationController.php
+++ app/Http/Controllers/Traveller/ReservationController.php
@@ -26,32 +26,6 @@ class ReservationController extends Controller {
 
     public function reservationList()
     {    
-        $reservations = Reservations::with(['reservedSuites.suite', 'reservedCompanions.companion'])
-            ->where('user_id', '=', Session::get('uid'))
-            ->get();
-        
-        $reserve_data = \DB::table('tb_reservations')->where('user_id',Session::get('uid'))->get();
-        $this->data['properties'] = properties::where('id',$reserve_data[0]->property_id)->get();
-        $this->data['reservations'] = $reservations;
-
-        $property_id =$this->data['properties'][0]->id;
-        $hotel_name = $this->data['properties'][0]->property_short_name;        
-        $this->data['booking_number'] = $reservations[0]->booking_number;
-        
-        $arrival_date = explode("-",$reserve_data[0]->checkin_date);
-        
-        $departure_date = explode("-",$reserve_data[0]->checkout_date);
-        
-        $this->data['arrive'] = $arrival_date[2];
-        $this->data['departure'] = $departure_date[2];
-        $this->data['year'] = $departure_date[0];
-        $this->data['month'] = date('M', $departure_date[1]);
-        $this->data['month_int'] = $departure_date[1];
-        
-
-        // $this->data['db'] = $this->databaseName();
-
-        // $this->data['policies'] = PropertyCategoryTypes::where('id',$suite_id)->first();        
         $file_name = 'frontend.themes.EC.reservation.receipt';
         return view($file_name, $this->data);   
     }
@@ -66,26 +40,11 @@ class ReservationController extends Controller {
         $hotel_name = $reservations[0]->property->property_name;        
         $booking_number = $reservations[0]->booking_number;
 
-        // $randomnum = mt_rand(0370,9999);        
-        $arrival_date = explode("-", $reservations[0]->checkin_date);        
-        $departure_date = explode("-", $reservations[0]->checkout_date);
-        
-        $arrive = $arrival_date[2];
-        $departure = $departure_date[2];
-        $year = $departure_date[0];
-        $month = date('M', $departure_date[1]);
-        $month_int = $departure_date[1];
         $trip_dates = CommonHelper::getDateRange($reservations[0]->checkin_date, $reservations[0]->checkout_date);
-        $db = $this->databaseName();
         $reserve_summary = view('frontend.themes.EC.reservation.partials.summary.reservation_summary',
             [   
                 'reservations' => $reservations,
                 'hotel_name' => $hotel_name,
-                'arrive' => $arrive,
-                'departure' => $departure,
-                'year' => $year,
-                'month' => $month,
-                'month_int' => $month_int,
                 'booking_number' => $booking_number,
                 'trip_dates' => $trip_dates
             ])->render();
@@ -94,35 +53,5 @@ class ReservationController extends Controller {
             'reserve_summary' => $reserve_summary
         ]);
     }
-
-    public function databaseName(){
-
-        if(request()->getHost() == 'development.emporium-voyage.com'){
-
-        $db = Config::get('app.EmporiumVoyage');   
-
-        }
-
-        if(request()->getHost() == 'emporium-safari.com'){
-
-        $db = Config::get('app.EmporiumSafari');   
-
-        }
-
-        if(request()->getHost() == 'emporium-spa.com'){
-
-        $db = Config::get('app.EmporiumSpa');   
-
-        }
-
-        if(request()->getHost() == 'emporium-islands.com'){
-
-        $db = Config::get('app.EmporiumIslands');   
-
-        }
-        return $db; 
-    }
-
-
 }
 ?>
