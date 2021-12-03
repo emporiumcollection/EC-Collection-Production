abaa3ef0bf9fe282300f141502892def998b4180
Mahesh Chavda <chavdamahesh@gmail.com>
2021-12-03 13:11:40 +0530

    Suite price corrected

--- app/Http/Traits/Property.php
+++ app/Http/Traits/Property.php
@@ -472,6 +472,10 @@ trait Property {
             if(!empty($property->suites)){
                 $suiteNameList = [];
                 foreach($property->suites as $sk => $suite){
+                    if(!$suite->status){
+                        unset($property->suites[$sk]);
+                        continue;
+                    }
                     $property->suites[$sk]->price = $this->getSuitePrice($suite->id);
                     $suiteNameList[] = ucwords($suite->cat_short_name);
                     if(!empty($suite->rooms->toArray())){
