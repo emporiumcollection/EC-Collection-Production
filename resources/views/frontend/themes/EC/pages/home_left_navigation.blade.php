383e290d78d2d3c80cfb68dd3c7e4120ee1ae948
Mahesh Chavda <chavdamahesh@gmail.com>
2021-12-13 17:25:44 +0530

    Dashboard changes

--- resources/views/frontend/themes/EC/pages/home_left_navigation.blade.php
+++ resources/views/frontend/themes/EC/pages/home_left_navigation.blade.php
@@ -6,18 +6,38 @@
           <div class="wiza-title">Hotels</div>
         </a>
       </li>
+      <?php if(\Config::get('app.currentdomain') != 'voyage'){?>
+      <li class="nav-item">
+        <a href="{{ \Config::get('app.voyagedomain') }}" class="nav-link">
+          <div><i class="ico ico-voyage"></i></div>
+          <div class="wiza-title">Voyage</div>
+        </a>
+      </li>
+      <?php }?>
+      <?php if(\Config::get('app.currentdomain') != 'spa'){?>
       <li class="nav-item">
         <a href="{{ \Config::get('app.spadomain') }}" class="nav-link">
           <div><i class="ico ico-spa"></i></div>
           <div class="wiza-title">Spas</div>
         </a>
       </li>
+      <?php }?>
+      <?php if(\Config::get('app.currentdomain') != 'safari'){?>
       <li class="nav-item">
         <a href="{{ \Config::get('app.safaridomain') }}" class="nav-link">
           <div><i class="ico ico-safari"></i></div>
           <div class="wiza-title">Safaris</div>
         </a>
       </li>
+      <?php }?>
+      <?php if(\Config::get('app.currentdomain') != 'islands'){?>
+      <li class="nav-item">
+        <a href="{{ \Config::get('app.islandsdomain') }}" class="nav-link">
+          <div><i class="ico ico-islands"></i></div>
+          <div class="wiza-title">Islands</div>
+        </a>
+      </li>
+      <?php }?>
       {{-- <li class="nav-item">
         <a href="#" class="nav-link">
           <div><i class="ico ico-fi-flight"></i></div>
@@ -36,12 +56,6 @@
           <div class="wiza-title">Villas</div>
         </a>
       </li> --}}
-      <li class="nav-item">
-        <a href="{{ \Config::get('app.islandsdomain') }}" class="nav-link">
-          <div><i class="ico ico-islands"></i></div>
-          <div class="wiza-title">Islands</div>
-        </a>
-      </li>
       <li class="nav-item">
         <a href="#" class="nav-link">
           <div><i class="ico ico-fi-magazine"></i></div>
