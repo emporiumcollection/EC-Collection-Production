436d1a0bec7703291aa000e40ac026119788ef31
Mahesh Chavda <chavdamahesh@gmail.com>
2022-01-27 19:35:43 +0530

    Fixed condtion for one domain

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -48,7 +48,7 @@ class Authenticate
 
                 $currentdomain = \Config::get('app.currentdomain');
                 $onelogindomain = \Config::get('app.onelogindomain');
-                if($currentdomain != 'onelogin'){
+                if($currentdomain != 'emporiumcollection'){
                     return Redirect::to($onelogindomain.'/check-one-login?referer='.request()->getSchemeAndHttpHost());
                 }
                 // return redirect()->guest('user/login');
