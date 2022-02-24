9e8dea2caee81d1811ea33800089ee1b5bea58a3
Mahesh Chavda <chavdamahesh@gmail.com>
2022-02-24 19:31:14 +0530

    Added product match

--- resources/views/match/roomdetail.blade.php
+++ resources/views/match/roomdetail.blade.php
@@ -9,14 +9,24 @@
     @endif
     <?php 
     $name = "";
+    $rooms_array = [];
+
     if(isset($rooms) && !empty($rooms)){
-        foreach ($rooms as $key => $value) { ?>
+        foreach ($rooms as $key => $value) {    
+            if(!in_array($value->room_id, $rooms_array) && !in_array($value->room_name, $rooms_array)){
+                $room_name = $value->room_name;
+                $room_id = $value->room_id;
+                $rooms_array[] = $room_id;
+                $rooms_array[] = $room_name;
+
+        ?>
         <div class="col-sm-6">
-            <input type="text" name="roomname" id="roomsName" value="{{$value->room_name}}" class="form-control">
+            <input type="text" name="roomname" id="roomsName" value="{{$room_name}}" class="form-control">
         </div>
         <div class="col-sm-6">
             <button name="view" class="btn btn-primary form-control">View</button>
         </div>     
-        <?php }    
+        <?php } 
+        }
     }?>    
 </div>
