7f397e7c746bab5dbac9bda208af141e84389413
Mahesh Chavda <chavdamahesh@gmail.com>
2022-02-24 16:49:29 +0530

    Added product match

--- resources/views/match/roomdetail.blade.php
+++ resources/views/match/roomdetail.blade.php
@@ -10,18 +10,13 @@
     <?php 
     $name = "";
     if(isset($rooms) && !empty($rooms)){
-        foreach ($rooms as $key => $value) {
-            foreach ($value->bed_configurations as $key => $val) {
-                foreach ($val->bed_types as $key => $data) { 
-                    $name .= $data->name_with_count.'.';  ?>
-                <div class="col-sm-6">
-                    <input type="text" name="roomname" id="roomsName" value="{{$name}}" class="form-control">
-                </div>
-                <div class="col-sm-6">
-                    <button name="view" class="btn btn-primary form-control">View</button>
-                </div>     
-            <?php } 
-            }    
-        }
+        foreach ($rooms as $key => $value) { ?>
+        <div class="col-sm-6">
+            <input type="text" name="roomname" id="roomsName" value="{{$value->room_name}}" class="form-control">
+        </div>
+        <div class="col-sm-6">
+            <button name="view" class="btn btn-primary form-control">View</button>
+        </div>     
+        <?php }    
     }?>    
 </div>
