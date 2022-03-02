17e923dbdd2ee8ac33701fc05697610cfa12cb6a
Mahesh Chavda <chavdamahesh@gmail.com>
2022-03-02 20:12:08 +0530

    Remove category fetch

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -80,7 +80,7 @@ class MatchController extends Controller
         $err = curl_error($curl);
 
         curl_close($curl);
-print 'here';exit;
+
         if ($err) {
             echo "cURL Error #:" . $err;echo "here";
         } else {
@@ -175,7 +175,7 @@ print 'here';exit;
                 CURLOPT_FOLLOWLOCATION => true,
                 CURLOPT_ENCODING => "",
                 CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 30,
+                CURLOPT_TIMEOUT => 60,
                 CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
                 CURLOPT_CUSTOMREQUEST => "GET",
                 CURLOPT_HTTPHEADER => [
@@ -278,7 +278,6 @@ print 'here';exit;
         } else {
             $policies = '';
             $response = json_decode($response);
-            print_r($response);exit;
             if(isset($response->policy)){
                 foreach ($response->policy as $key => $value) {
                     $policies .= PHP_EOL;
