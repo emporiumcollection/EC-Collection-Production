cc5334c86264b51b1d47aa6bb1667dacd02aa59c
Mahesh Chavda <chavdamahesh@gmail.com>
2022-04-12 17:16:54 +0530

    error fixing in safari

--- resources/views/properties/settings_cats.blade.php
+++ resources/views/properties/settings_cats.blade.php
@@ -40,7 +40,6 @@
 		<div class="sbox  "> 
 			<div class="sbox-title">@if(!empty($property_data)) {{$property_data->property_name}} @endif <a href="{{URL::to('properties/update/'.$pid)}}" class="tips btn btn-xs btn-primary pull-right" title="" data-original-title="Property Management"><i class="fa fa-edit"></i>&nbsp;Property Management</a></div>
 				<div class="sbox-content">
-				<?php print_r($cat_types);exit; ?> 
 					@if(!empty($cat_types))					
 					{{--*/ $c=1; /*--}}
 						@foreach($cat_types as $cat)
